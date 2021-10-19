3da6c9d331c25f340d1f25e761b3ad939a54bab2
MarlonMoreno <marlonmoreno894@gmail.com>
2021-10-19 15:50:55 -0500

    Add model associations and validations to Manga

--- app/models/manga.rb
+++ app/models/manga.rb
@@ -1,2 +1,12 @@
 class Manga < ApplicationRecord
+    has_many :manga_lists
+    has_many :manga_genres
+    has_many :lists, through: :manga_lists
+    has_many :genres, thorugh: :manga_genres
+    validates :name, presence: true
+    validates :image_url, presence: true
+    validates :synopsis, presence: true
+    validates :score, presence: true
+    validates :date_start, presence: true
+    validates :date_end, presence: true
 end
