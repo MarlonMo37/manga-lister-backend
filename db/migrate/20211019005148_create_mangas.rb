59d0f6c623400bb5713a34d25bb22a77f406ea7c
MarlonMoreno <marlonmoreno894@gmail.com>
2021-10-19 12:39:43 -0500

    Add columns to Manga

--- db/migrate/20211019005148_create_mangas.rb
+++ db/migrate/20211019005148_create_mangas.rb
@@ -1,6 +1,12 @@
 class CreateMangas < ActiveRecord::Migration[6.1]
   def change
     create_table :mangas do |t|
+      t.string :name
+      t.string :image_url
+      t.text :synopsis
+      t.float :score
+      t.string :date_start
+      t.string :date_end
 
       t.timestamps
     end
