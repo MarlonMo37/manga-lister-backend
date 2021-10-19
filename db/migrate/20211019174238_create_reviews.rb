02a45f7beff99703be792f1837648a4bb437f269
MarlonMoreno <marlonmoreno894@gmail.com>
2021-10-19 14:49:17 -0500

    Add columns to List

--- db/migrate/20211019174238_create_reviews.rb
+++ db/migrate/20211019174238_create_reviews.rb
@@ -1,6 +1,9 @@
 class CreateReviews < ActiveRecord::Migration[6.1]
   def change
     create_table :reviews do |t|
+      t.string :written_review
+      t.integer :rating
+      t.integer :movie_id
 
       t.timestamps
     end
