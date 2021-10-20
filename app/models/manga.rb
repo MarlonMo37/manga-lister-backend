ff02298c08fd6aea9e53f835b2c6ab6c2eda9626
MarlonMoreno <marlonmoreno894@gmail.com>
2021-10-20 14:40:01 -0500

    Make class function that returns ids of popular mangas

--- app/models/manga.rb
+++ app/models/manga.rb
@@ -3,11 +3,21 @@ class Manga < ApplicationRecord
     has_many :manga_lists
     has_many :manga_genres
     has_many :lists, through: :manga_lists
-    has_many :genres, thorugh: :manga_genres
+    has_many :genres, through: :manga_genres
     validates :name, presence: true
     validates :image_url, presence: true
     validates :synopsis, presence: true
     validates :score, presence: true
     validates :date_start, presence: true
     validates :date_end, presence: true
+
+    def self.get_manga_ids
+        ids_url = "https://api.jikan.moe/v3/top/manga"
+        url = URI(ids_url)
+
+        response = Net::HTTP.get(url)
+        information = JSON.parse(response)
+
+        information["top"].map {|manga| manga["mal_id"]}
+    end
 end
